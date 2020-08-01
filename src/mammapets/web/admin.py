c88af06c82a1f8b0e05f28b33d85e516b7df298f
Quispe Ulloa,Karla Astrid,BARCELONA,IT Marketing, Sales and eBusiness <KarlaAstrid.QuispeUlloa@es.nestle.com>
2020-08-01 18:31:16 +0200

    Add admin backoffice

--- src/mammapets/web/admin.py
+++ src/mammapets/web/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib import admin
+
+from .models import Pet, Client, MammaPet
+
+admin.site.register(Pet)
+admin.site.register(Client)
+admin.site.register(MammaPet)
