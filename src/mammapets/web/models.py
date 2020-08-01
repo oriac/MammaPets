3a0621491b62d4f7e4f3163798c5315173400b9f
Quispe Ulloa,Karla Astrid,BARCELONA,IT Marketing, Sales and eBusiness <KarlaAstrid.QuispeUlloa@es.nestle.com>
2020-08-01 17:31:05 +0200

    Add model classes

--- src/mammapets/web/models.py
+++ src/mammapets/web/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Person(models.Model):
+    username = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+
+
+class Client(Person):
+    address = models.CharField(max_length=200)
+
+
+class Pet(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    owner = models.ForeignKey(Client, on_delete=models.CASCADE)
+
+
+class MammaPet(Person):
+    phone = models.CharField(max_length=200)
+
+
+class Contract(models.Model):
+    start_date = models.DateTimeField('date started')
+    end_date = models.DateTimeField('date started')
+    pet = models.ForeignKey(Pet, on_delete=models.CASCADE)
+    mamma_pet = models.ForeignKey(MammaPet, on_delete=models.CASCADE)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    price = models.FloatField(default=0)
+    # TODO: CHANGE TO ENUM
+    status = models.CharField(max_length=200)
